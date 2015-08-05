8269f6798250411ef9b52e07d7edc9db39aaa33d
Daniel Fly Nygaard <danielflynygaard@gmail.com>
2015-08-05 18:39:31 +0200

    Virker stadig ikke!

--- routes/manage.js
+++ routes/manage.js
@@ -5,11 +5,11 @@ var router = express.Router();
 /* Managing YubiKey devices. */
 router.get("/", function (req, res, next)
 {
-    var u2fReq = u2f.request("http://localhost:3000");
+    var u2fReq = u2f.request("http://localhost:3000", 'test');
     req.session.authRequest = u2fReq;
 
     if(req.session.username)
-        res.render("manage", {username: req.session.username, u2freq: u2fReq});
+        res.render("manage", {username: req.session.username, u2freq: JSON.stringify(u2fReq)});
     else
         res.render("manage", {error: 1, u2freq: JSON.stringify(u2fReq)});
 });
